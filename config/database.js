f9efaf71222023a125a9c448a4fc01106e098fbe
Ferdi Firdaus Ega Pratama <ferdifirdaus157@gmail.com>
2024-10-31 01:40:58 +0700

    Adding auth.js, database.js, and the createauth migration.

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: "localhost",
+    database: process.env.DB,
+    host: "127.0.0.1",
     dialect: "postgres",
-    port: 5432,
   },
 };
