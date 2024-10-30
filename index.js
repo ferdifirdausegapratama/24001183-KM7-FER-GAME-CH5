53600576bcf4797bef6332096d0ba6b55755e83a
Ferdi Firdaus Ega Pratama <ferdifirdaus157@gmail.com>
2024-10-30 09:26:35 +0700

    Change index.js code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 
 const express = require("express");
-//const morgan = require("morgan");
-//const cors = require("cors");
-//const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 //const ApiError = require("./utils/ApiError");
 const errorHandler = require("./controllers/ErrorController");
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(morgan("dev"));
 
-app.use(router);
+//app.use(router);
 
 app.all("*", (req, res, next) => {
   next(new ApiError("Routes doesn't exist", 404));
